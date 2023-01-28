f90dd1a57fc00f8c3a5d5226e401c96be7b62a52
Osama Al-mamari <yemencoder@gmail.com>
2023-01-28 21:50:43 +0300

    Cubit

--- basketball_counter_app/lib/cubit/counter_cubit.dart
+++ basketball_counter_app/lib/cubit/counter_cubit.dart
@@ -5,5 +5,17 @@ import 'counterState.dart';
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterAIncrementState());
 
+  int teamAPoints = 0;
 
+  int teamBPoints = 0;
+
+  void teamPIncrement({required String team, required int points}) {
+    if (team == "A") {
+      teamAPoints += points;
+      emit(CounterAIncrementState());
+    } else {
+      teamBPoints += points;
+      emit(CounterBIncrementState());
+    }
+  }
 }
